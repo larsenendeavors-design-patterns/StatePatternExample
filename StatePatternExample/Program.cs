8108f144111029df00f08cbdad726223ca7e182c
Nicholas Larsen <nlarsen@access-healthservices.com>
2021-10-11 11:45:59 -0500

    Initial commit

---
+++ StatePatternExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatePatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
